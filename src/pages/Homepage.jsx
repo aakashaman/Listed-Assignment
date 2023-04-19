673c441d559b1cd64d5b617014fb1146b02c24bc
aakashaman <aakashaman9931@gmail.com>
2023-04-19 18:36:41 +0530

    hel

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,7 +4,6 @@ import Linechart from "../components/chart/Linechart";
 import Piechart from "../components/chart/Piechart";
 import ScheduleCard from "../components/schedulecard/Schedule";
 import Sidebar from "../components/sidebar/Sidebar";
-import LoginPage from "./Login";
 
 export default function Homepage() {
   return (
@@ -22,7 +21,7 @@ export default function Homepage() {
       <div style={{display:"flex",justifyContent:"space-between"}}>
       <Piechart/>
       <ScheduleCard/>
-      <LoginPage/>
+      
       </div>
       
       </div>
