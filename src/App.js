60e736b10a3c3aa979281a25b9e1f510d2bc011c
aakashaman <aakashaman9931@gmail.com>
2023-04-19 18:29:59 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
+import { useState } from 'react';
 import Homepage from './pages/Homepage';
+import LoginPage from './pages/Login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <div >
       <div>
+      {isLoggedIn ? (
         <Homepage/>
+        ) : (
+          <LoginPage onLogin={() => setIsLoggedIn(true)} />
+        )}
 
       </div>
       
